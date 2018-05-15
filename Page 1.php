25e57615fd64f6c9606349fbf363112a615d290a
Yaneli <yoestrad@uci.edu>
2018-05-15 13:33:29 -0700

    Add files via upload

---
+++ Page 1.php
@@ -0,0 +1,114 @@
+<!
+Light Grey: #A7AEB1
+Light Blue Background: #dfedf0
+>
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta charset="utf-8">
+
+	<title>Head Start Checklist</title>
+	<meta name="description" content="Head Start">
+	<meta name="author" content="Sour Patch Kids 117">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style4.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+	<![endif]-->
+
+	<style type="text/css">
+		body {background-color: #dfedf0;}
+
+		.container{
+			background-color: white;
+		}
+
+		.jumbotron{
+			background-color: #A7AEB1;
+			background-image: url("images/hb-pier-ca2.png");
+			background-repeat: no-repeat;
+			/*-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover; */
+			color: white;
+			margin-top: 20px;
+			}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Head Start</h1>
+			<p>Registration Checklist</p>
+		</div>
+	</div>
+
+	<div class="container">
+	<h2>About Head Start</h2>
+	<p>Head Start is a comprehensive child development program designed to meet the educational, emotional, social, health, and nutritional needs of children and families.
+		 The program encompasses much more than what is generally thought of as a preschool program. The overall goal of Head Start is to bring about a greater degree of 
+		"social competence" to children and their families. Social competence refers to the everyday effectiveness in dealing with both present environment and later responsibilities 
+		in school and life. Children and families leave Head Start more confident in their own abilities and ready to succeed. 
+	</p>
+		<p><a class="button" href= "http://www.ochsinc.org/" target = "_blank" role="button">Learn more</a></p>
+	</div>
+
+	<div class = "container">
+	<hr class = "style8">
+	</div>
+	
+	<div class="container">
+	<h2>Services Provided</h2>
+	<p>
+		OR add to previous section Head Start provides a variety of services ----
+	</p>
+	</div>
+
+	<div class = "container">
+	<hr class = "style8">
+	</div>
+
+	<div class="container">
+	  <h2>Login</h2>
+	  <p>To view your child's registration status please enter your child's ID number and birthdate below.</p>
+	  <p>If you are unsure of your children's ID number please check the email input in the registration form for a confirmation email which states the child specific ID.</p>
+		<br>
+	  <form class="form-inline" action="action_page2.php">
+
+	      <label for="cid">Child ID:</label>
+	      <input type="text" class="form-control" id="cid" placeholder="Enter Child ID" name="cid">
+
+	      <label for="pcd">Birthdate:</label>
+	      <input type="password" class="form-control" id="pcd" placeholder="MMDDYYYY" name="pcd">
+
+	    <button type="submit" class="btn btn-default"> Submit </button>
+	  </form>
+	<br>
+	<p> Please note that for families with more than one child each child's status must be checked individually.</p>
+	 <p>If you have not completed an online or in-person registration form please do so using the following link or visit an OC Head Start office for more information.</p>
+	  <p><a class="button" href= "http://www.ochsinc.org/" target = "_blank" role="button">Register</a></p>
+
+		</br></br></br>
+	<script src="js/scripts.js"></script>
+
+      <footer>
+
+        <details>
+      	
+          2501 S. Pullman Street, Suite 100</br>
+		  Santa Ana, CA 92705</br>
+		  Phone: (714) 241-8920</br>
+		  Fax: (949) 596-8291</br>
+	<summary>&copy; Copyright 2008-2018 by Orange County, Head Start Inc.</summary>
+        </details>
+      </footer>
+	</div>
+
+</body>
+</html>
---
+++ action_page2.php
@@ -0,0 +1,115 @@
+﻿<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta charset="utf-8">
+
+	<title>Head Start Checklist</title>
+	<meta name="description" content="Head Start">
+	<meta name="author" content="Sour Patch Kids 117">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style4.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+	<![endif]-->
+
+	<style type="text/css">
+		body {background-color: #dfedf0;}
+
+		.container{
+			background-color: white;
+		}
+
+		.jumbotron{
+			background-color: #A7AEB1;
+			background-image: url("images/hb-pier-ca2.png");
+			background-repeat: no-repeat;
+			/*-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover; */
+			color: white;
+			margin-top: 20px;
+			}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Head Start</h1>
+			<p>Registration Checklist</p>
+		</div>
+	</div>
+
+	<div class = "container">
+	<h2>The Head Start Process</h2>
+	<p>Welcome to Head Start, in order to complete the registration process the Federal Government requires families to submit a number of documents regarding both the child
+		and household. If a specific document, such as foster child documentation, does not apply please ignore the document. If you are unsure if a document applies please
+		check our FAQ page for more information.</p>
+	<p> The registration and completion process has three steps and this page will be updated as the status of the child changes.</p>
+	<p>1. New Applicant : A new applicant is one who has just begun the registration process.</p>
+	<p>2. Waitlisted Applicant : Once all required documents are submitted a child will be placed into a waitlist in which they shall remain until a spot opens in the program.</p>
+	<p>3. Enrolled : Once a spot opens and the child is officially enrolled they can begin attending the program(?). Please note to complete enrollment an orientation meeting must be scheduled
+		with a representative at an OC Head Start office. For more information on orientation please view out FAQ page or contact the OC Head Start office closest to you by using our
+		locator service. -insert link-</p>
+	</div>
+
+
+	<div class="container">
+		<div>
+			<h2>Label Legend</h2>
+			<span class="label label-success">Received</span> : The document has been received with no errors.</br>
+			<span class="label label-warning">Error</span> : The document was received but has errors, please refer to the "notes"for more information.</br>
+			<span class="label label-danger">Missing</span> : The document was requested but has not been received.</br>
+		</div>
+
+		</br>
+
+		</br>
+		<h2>Child Documents : #########</h2>
+		<div>
+			<p><span class="label label-success">Received</span> || Hospital Birth Certificate</p>
+			<p><span class="label label-warning">Error</span> || Immunization Record - Missing</p>
+			<p><span class="label label-success">Received</span> || Physical Exam</p>
+			<p><span class="label label-success">Received</span> || Dental Exam</p>
+			<p><span class="label label-danger">Missing</span> || Prescription Medication Information Request Letter </p>
+			<p><span class="label label-danger">Missing</span> || Food Substitution Form</p>
+			<p><span class="label label-warning">Error</span> || Milk Substitution Form</p>
+		</div>
+		</br>
+
+		<h2>Parent 1 Documents</h2>
+		<div>
+			<p><span class="label label-success">Received</span> || 1040 Income Tax Return 2017</p>
+			<p><span class="label label-warning">Error</span> || Proof of 20+ hour work week (Monday – Friday) </p>
+		</div>
+		</br>
+		
+		<h2>Parent 2 Documents*</h2>
+		<div>
+			<p><span class="label label-warning">Error</span> || Proof of 20+ hour work week (Monday – Friday) </p>
+		</div>
+		</br>
+		
+		<script src="js/scripts.js"></script>
+		<p>*Note: Please ignore if a single parent household.</p>
+
+		<footer>
+		<details>
+			2501 S. Pullman Street, Suite 100</br>
+			Santa Ana, CA 92705</br>
+			Phone: (714) 241-8920</br>
+			Fax: (949) 596-8291</br>
+			<summary>&copy; Copyright 2008-2018 by Orange County, Head Start Inc.</summary>
+		</details>
+	</footer>
+	</div>
+
+
+
+</body>
+</html>
