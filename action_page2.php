8cbabe2d6683c3856bd31b41b2c892c8826bf931
Yaneli <yoestrad@uci.edu>
2018-05-22 11:09:54 -0700

    Update action_page2.php

--- action_page2.php
+++ action_page2.php
@@ -1,111 +1,18 @@
-<!
-Leaf Green: #436361
-Light Grey: #A7AEB1
-Light Blue Background: #dfedf0
->
-
-<!DOCTYPE html>
-<html lang="en-US">
-	<head>
-	<meta charset="utf-8">
-
-	<title>Head Start Checklist</title>
-	<meta name="description" content="Head Start">
-	<meta name="author" content="Sour Patch Kids 117">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/style4.css">
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-	<!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-	<![endif]-->
-
-	<style type="text/css">
-		body {background-color: #dfedf0;}
-		h3 {color : white;}
-	
-		.container{
-			background-color: white;
-		}
-		.footer{background-color: #A7AEB1;
-			color: white;
-		}
-
-		.jumbotron{
-			background-color: white;
-			background-image: url("images/HS-Logo-Custom.png");
-			background-position: center center;
-			background-repeat: no-repeat;
-			background-size: cover;
-			/*-webkit-background-size: cover;
-			-moz-background-size: cover;
-			-o-background-size: cover;
-			background-size: cover; */
-			background: contain;
-			width : 100%;
-			height : 900; 
-			margin-top: 20px;
-
-			}
-
-		.button {
-   			 background-color: #436361; 
-			 -webkit-transition-duration: 0.4s; /* Safari */
-             		 transition-duration: 0.2s;
-   			 border: none;
-   			 color: white;
-    			 padding: 12px 28px;
-   			 text-align: center;
-			 border-radius: 4px;
-  			 text-decoration: none;
-   			 display: inline-block;
-   			 font-size: 16px;
-			 font-weight: bold;
-			}
-
-		.button:hover {
-			 text-decoration: none;
-   			 color: white;
-			 font-weight: bold;
-			}
-
-		a:link {color : #436361;
-			text-decoration : none}
-
-		a:hover {color : #436361;
-			font-weight : bold;
-			text-decoration : none}
-	</style>
-
-</head>
-<body>
-	<div class = "container">
-		<div class="jumbotron container-fluid">
-		<h3>..</h3>
-		<h3>..</h3>
-		<h3>..</h3>
-		</div>
-	</div>
-
-	<div class = "container">
-	<hr class = "style8">
-	</div>
-
-
-	<div class = "container">
-	<h2>The Head Start Process</h2>
-	<p>Welcome to Head Start, in order to complete the registration process the Federal Government requires families to submit a number of documents regarding both the child
-		and household. If a specific document, such as foster child documentation, does not apply please ignore the document. If you are unsure if a document applies please
+ot apply please ignore the document. If you are unsure if a document applies please
 		check our FAQ page for more information.</p>
+
+
 	<p> The registration and completion process has three steps and this page will be updated as the status of the child changes.</p>
 	<p>1. New Applicant : A new applicant is one who has just begun the registration process.</p>
-	<p>2. Waitlisted Applicant : Once all required documents are submitted a child will be placed into a waitlist in which they shall remain until a spot opens in the program.</p>
-	<p>3. Enrolled : Once a spot opens and the child is officially enrolled they can begin attending the program(?). Please note to complete enrollment an orientation meeting must be scheduled
-		with a representative at an OC Head Start office. For more information on orientation please view out FAQ page or contact the OC Head Start office closest to you by using our
-		locator service. -insert link-</p>
+	<p>2. Waitlisted Applicant : Once all required documents are submitted a child will be placed into a waitlist</p><p>in which they shall remain until a spot opens in the program.</p>
+	<p>3. Enrolled : Once a spot opens and the child is officially enrolled they can begin attending the program.</p> <p> Please note to complete enrollment an orientation meeting must be 		scheduled with a representative at an OC Head Start office.</p>
+	<p>For more information on orientation please view out FAQ page or contact the OC Head Start office closest to you by using our
+		locator service. </p>
+	<a class="button" href= "" >Location Finder</a>
+	
+	<form class="form-inline" action="FAQ_page.php">
+	<button type="submit" class="btn btn-default"> FAQ Page </button>
+
 	</div>
 
 	<div class = "container">
@@ -150,16 +57,21 @@ Light Blue Background: #dfedf0
 		<script src="js/scripts.js"></script>
 		<p>*Note: Please ignore if a single parent household.</p>
 
+		<div class = "container">
+		<hr class = "style8">
+		</div>
+
 		<div class = "footer">
-		<details>
-			Main Office</br>
-			2501 S. Pullman Street, Suite 100</br>
-			Santa Ana, CA 92705</br>
-			Phone: (714) 241-8920</br>
-			Fax: (949) 596-8291</br>
-			<summary>&copy; Copyright 2008-2018 by Orange County, Head Start Inc.</summary>
-	
-		</details>
+
+	<summary>
+      		  Main Office</br>	
+        	  2501 S. Pullman Street, Suite 100</br>
+		  Santa Ana, CA 92705</br>
+		  Phone: (714) 241-8920</br>
+		  Fax: (949) 596-8291</br>
+	&copy; Copyright 2008-2018 by Orange County, Head Start Inc.</summary>
+
+	</footer>
 	</div>
 
 
